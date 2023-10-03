fd068090669378e2b90ad7d6d17d9a6bd326191a
Ulrich Eck <ueck@net-labs.de>
2023-10-03 09:24:04 +0200

    implemented a workaround for uwp on armv8 - conan somehow messes with the environment/cmake settings so that cross-compiling does not work correctly.

--- conanfile.py
+++ conanfile.py
@@ -1,13 +1,12 @@
 import os
 from conan import ConanFile
-from conan.tools.files import update_conandata, copy, chdir, mkdir, collect_libs, replace_in_file
-from conan.tools.env import VirtualRunEnv
+from conan.tools.files import update_conandata, copy, rm, chdir, mkdir, collect_libs, replace_in_file
+from conan.tools.env import VirtualRunEnv, VirtualBuildEnv
 from conan.tools.scm import Git
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout, CMakeDeps
+from conan.tools.microsoft import VCVars
 
 class ZenohCConan(ConanFile):
-    python_requires = "camp_common/0.5@camposs/stable"
-    python_requires_extend = "camp_common.CampCMakeBase"
 
     name = "zenoh-c"
     version = "0.7.2-rc"
@@ -24,8 +23,8 @@ class ZenohCConan(ConanFile):
 
     def export(self):
         update_conandata(self, {"sources": {
-            "commit": "{}".format(self.version),
-            "url": "https://github.com/eclipse-zenoh/zenoh-c.git"
+            "commit": "master",  # "{}".format(self.version),
+            "url": "https://github.com/TUM-CAMP-NARVIS/zenoh-c.git"
             }}
             )
 
@@ -39,7 +38,31 @@ class ZenohCConan(ConanFile):
         if self.settings.os == "WindowsStore" and self.settings.arch == "armv8":
             tc.cache_variables["ZENOHC_CARGO_CHANNEL"] = "nightly"
             tc.cache_variables["ZENOHC_CUSTOM_TARGET"] = "aarch64-uwp-windows-msvc"
-            tc.cache_variables["ZENOHC_CARGO_FLAGS"] = "-Z build-std=panic_abort,std"
+            tc.cache_variables["ZENOHC_CARGO_FLAGS"] = "-Zbuild-std=panic_abort,std"
             tc.cache_variables["ZENOHC_BUILD_WITH_SHARED_MEMORY"] = False
 
 
+
+    def build(self):
+        if self.settings.os == "WindowsStore" and self.settings.arch == "armv8":
+            be = VirtualRunEnv(self)
+            with be.vars(scope="run").apply():
+                # try hardcoded for now ..
+                # conan-cmake seems to mess with some environment/settings
+                self.run("cargo +nightly build -Zbuild-std=panic_abort,std --release --features=logger-autoinit --target=aarch64-uwp-windows-msvc")
+        else:
+            super().build()
+
+
+    def package(self):
+        if self.settings.os == "WindowsStore" and self.settings.arch == "armv8":
+            bin_path = os.path.join(self.source_folder, "target", 'aarch64-uwp-windows-msvc')
+            # clean up before recursive copy
+            rm(self, "*.dll", os.path.join(bin_path, "deps"))
+            rm(self, "*.lib", os.path.join(bin_path, "deps"))
+            # Manually copy the files in the target (needs to be adapted if debug build is enabled ..)
+            copy(self, "zenohc.dll", bin_path, os.path.join(self.package_folder, "bin"), keep_path=False)
+            copy(self, "zenohc.lib", bin_path, os.path.join(self.package_folder, "lib"), keep_path=False)
+            copy(self, "*.h", os.path.join(self.source_folder, "include"), os.path.join(self.package_folder, "include"))
+        else:
+            super().package()
