f03964e4a7588787888940804358628a667e981f
Ulrich Eck <ulrich.eck@tum.de>
2026-03-27 10:50:21 +0100

    fix macos rpath issue

--- conanfile.py
+++ conanfile.py
@@ -1,22 +1,23 @@
 import os
 from textwrap import dedent
+
 from conan import ConanFile
-from conan.tools.files import update_conandata, copy, rm, chdir, mkdir, collect_libs, replace_in_file, save, rename
-from conan.tools.env import VirtualRunEnv, VirtualBuildEnv
-from conan.tools.scm import Git
-from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout, CMakeDeps
-from conan.tools.microsoft import VCVars
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.env import VirtualRunEnv
+from conan.tools.files import copy, rm, save, update_conandata
 from conan.tools.layout import basic_layout
+from conan.tools.scm import Git
+
 
 class ZenohCConan(ConanFile):
 
     name = "zenoh-c"
     version = "1.3.4"
-    
+
     license = "Apache License 2.0"
     author = "Ulrich Eck"
     url = "https://github.com/TUM-CONAN/conan-zenohc.git"
-    description = "Recipe to build zenoh-c using conan"    
+    description = "Recipe to build zenoh-c using conan"
 
     settings = "os", "compiler", "build_type", "arch"
 
@@ -32,11 +33,15 @@ class ZenohCConan(ConanFile):
     exports_sources = "CMakeLists.txt"
 
     def export(self):
-        update_conandata(self, {"sources": {
-            "commit": "{}".format(self.version),
-            "url": "https://github.com/eclipse-zenoh/zenoh-c.git"
-            }}
-            )
+        update_conandata(
+            self,
+            {
+                "sources": {
+                    "commit": "{}".format(self.version),
+                    "url": "https://github.com/eclipse-zenoh/zenoh-c.git",
+                }
+            },
+        )
 
     def source(self):
         git = Git(self)
@@ -44,15 +49,26 @@ class ZenohCConan(ConanFile):
         git.clone(url=sources["url"], target=self.source_folder)
         git.checkout(commit=sources["commit"])
 
-
     @property
     def is_win(self):
         return self.settings.os == "Windows" or self.settings.os == "WindowsStore"
 
     @property
+    def is_linux(self):
+        return self.settings.os == "Linux"
+
+    @property
+    def is_macos(self):
+        return self.settings.os == "Macos"
+
+    @property
+    def is_unix_like(self):
+        return self.is_linux or self.is_macos
+
+    @property
     def is_uwp_armv8(self):
         return self.settings.os == "WindowsStore" and self.settings.arch == "armv8"
-    
+
     @property
     def is_win_x64(self):
         return self.settings.os == "Windows" and self.settings.arch == "x86_64"
@@ -62,18 +78,17 @@ class ZenohCConan(ConanFile):
         return self.settings.os == "Android" and self.settings.arch == "armv8"
 
     def generate(self):
-
         tc = CMakeToolchain(self)
+
         def add_cmake_option(option, value):
             var_name = "{}".format(option).upper()
             value_str = "{}".format(value)
-            var_value = "ON" if value_str == 'True' else "OFF" if value_str == 'False' else value_str
+            var_value = "ON" if value_str == "True" else "OFF" if value_str == "False" else value_str
             tc.variables[var_name] = var_value
 
         for option, value in self.options.items():
             add_cmake_option(option, value)
 
-        # not used as workaround below, but in theory that would be the right settings
         if self.is_uwp_armv8:
             tc.cache_variables["ZENOHC_CARGO_CHANNEL"] = "nightly"
             tc.cache_variables["ZENOHC_CUSTOM_TARGET"] = "aarch64-uwp-windows-msvc"
@@ -89,7 +104,6 @@ class ZenohCConan(ConanFile):
         tc.generate()
 
         deps = CMakeDeps(self)
-
         deps.generate()
 
     def layout(self):
@@ -101,44 +115,46 @@ class ZenohCConan(ConanFile):
     def patch_sources(self):
         if self.is_uwp_armv8:
             self.output.info("Patching sources for UWP.")
-            # use nightly toolchain
-            #rename(self, os.path.join(self.source_folder, "rust-toolchain"), os.path.join(self.source_folder, "disabled-rust-toolchain"))
-            save(self, os.path.join(self.source_folder, "rust-toolchain.toml"), 
-                dedent("""
-                [toolchain]
-                # +nightly is required to enable the build-std feature. The following specifies that the
-                # latest version be used. It can be pinned here to a specific version, e.g.
-                # "nightly-2021-08-12".
-                channel = "nightly"
-                components = ["rust-src"]
-                """))
-            # add specific version of ring that is compatible with uwp
-            save(self, os.path.join(self.source_folder, "Cargo.toml"), 
-                dedent("""
-                
-                [patch.crates-io]
-                ring = { git = "https://github.com/awakecoding/ring", branch = "0.16.20_alpha" }
-                """), append=True)
+            save(
+                self,
+                os.path.join(self.source_folder, "rust-toolchain.toml"),
+                dedent(
+                    """
+                    [toolchain]
+                    # +nightly is required to enable the build-std feature. The following specifies that the
+                    # latest version be used. It can be pinned here to a specific version, e.g.
+                    # "nightly-2021-08-12".
+                    channel = "nightly"
+                    components = ["rust-src"]
+                    """
+                ),
+            )
+            save(
+                self,
+                os.path.join(self.source_folder, "Cargo.toml"),
+                dedent(
+                    """
 
-            # replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
-            #     """${CMAKE_CURRENT_SOURCE_DIR}/rust-toolchain""",
-            #     """${CMAKE_CURRENT_SOURCE_DIR}/rust-toolchain.toml""")
-
-            # replace_in_file(self, os.path.join(self.source_folder, "Cargo.toml"),
-            #     'async-std = "=1.12.0"',
-            #     'ahash = "0.8.9"\nasync-std = "=1.12.0"')
+                    [patch.crates-io]
+                    ring = { git = "https://github.com/awakecoding/ring", branch = "0.16.20_alpha" }
+                    """
+                ),
+                append=True,
+            )
 
     def build(self):
         self.patch_sources()
         if self.is_uwp_armv8:
-            be = VirtualRunEnv(self)
-            with be.vars(scope="run").apply():
-                # try hardcoded for now ..
-                # conan-cmake seems to mess with some environment/settings
+            build_env = VirtualRunEnv(self)
+            with build_env.vars(scope="run").apply():
                 rel_flag = ""
                 if self.settings.build_type == "Release":
                     rel_flag = " --release"
-                self.run("cargo +nightly build -Zbuild-std=panic_abort,std{0} --features=logger-autoinit --target=aarch64-uwp-windows-msvc".format(rel_flag))
+                self.run(
+                    "cargo +nightly build -Zbuild-std=panic_abort,std{0} --features=logger-autoinit --target=aarch64-uwp-windows-msvc".format(
+                        rel_flag
+                    )
+                )
         else:
             cmake = CMake(self)
             cmake.configure()
@@ -150,37 +166,54 @@ class ZenohCConan(ConanFile):
             name += "d"
         return name
 
+    def _fix_macos_shared_library_install_name(self):
+        if not self.is_macos or not self.options.shared:
+            return
+
+        library_name = "lib{}.dylib".format(self._zenoh_lib_name())
+        library_path = os.path.join(self.package_folder, "lib", library_name)
+        if not os.path.exists(library_path):
+            self.output.warning("Expected shared library not found at {}".format(library_path))
+            return
+
+        self.output.info("Rewriting install name for {}".format(library_name))
+        self.run('install_name_tool -id @rpath/{0} "{1}"'.format(library_name, library_path))
+
+    def _prune_unix_library_artifacts(self):
+        if not self.is_unix_like:
+            return
+
+        library_folder = os.path.join(self.package_folder, "lib")
+        if self.options.shared:
+            rm(self, "*.a", library_folder, recursive=False)
+        else:
+            rm(self, "*.so", library_folder, recursive=False)
+            rm(self, "*.dylib", library_folder, recursive=False)
+
     def package(self):
         if self.is_win:
             bin_path = None
             folder = "release" if self.settings.build_type == "Release" else "debug"
             if self.is_uwp_armv8:
-                bin_path = os.path.join(self.source_folder, "target", 'aarch64-uwp-windows-msvc', folder)
+                bin_path = os.path.join(self.source_folder, "target", "aarch64-uwp-windows-msvc", folder)
             if self.is_win_x64:
                 bin_path = os.path.join(self.build_folder, folder, "target", folder)
 
             if bin_path is not None:
-                # clean up before recursive copy
                 rm(self, "*.dll", os.path.join(bin_path, "deps"))
                 rm(self, "*.lib", os.path.join(bin_path, "deps"))
-                # Manually copy the files in the target (needs to be adapted if debug build is enabled ..)
-
                 copy(self, "*.dll", bin_path, os.path.join(self.package_folder, "bin"), keep_path=False)
                 copy(self, "*.lib", bin_path, os.path.join(self.package_folder, "lib"), keep_path=False)
                 copy(self, "*.h", os.path.join(self.build_folder, folder, "include"), os.path.join(self.package_folder, "include"))
                 copy(self, "*.h", os.path.join(self.source_folder, "include"), os.path.join(self.package_folder, "include"))
             else:
                 self.output.error("Unknown Windows platform - install incomplete !!!")
-        else:
-            cmake = CMake(self)
-            cmake.install()
+            return
 
-            if self.settings.os == "Linux":
-                if self.options.shared:
-                    rm(self, "*.a", os.path.join(self.package_folder, "lib"), recursive=False)
-                else:
-                    rm(self, "*.so", os.path.join(self.package_folder, "lib"), recursive=False)
-            # what about other platforms?
+        cmake = CMake(self)
+        cmake.install()
+        self._prune_unix_library_artifacts()
+        self._fix_macos_shared_library_install_name()
 
     def package_info(self):
         self.cpp_info.libs = [self._zenoh_lib_name()]
